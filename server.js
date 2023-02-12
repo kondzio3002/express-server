202f09b5f9ff1dbe1997a9373fdbac157cc27982
Konrad Brożyna <kondzio3002@gmail.com>
2023-02-12 15:42:13 +0100

    Add new endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const uuid = require('uuid').v4;
+const db = require('./db');
 
 const app = express();
 
@@ -8,47 +9,104 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-  { id: 3, author: 'Adam Snow', text: 'Successful cooperation!' },
-  { id: 4, author: 'Danny Nitro', text: 'It promise great cooperation.' },
-  { id: 5, author: 'Angelina Nobel', text: 'Make money!' },
-  { id: 6, author: 'Anna Maj', text: 'Good job.' },
-];
-
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/random', (req, res) => {
-  res.json(db[Math.floor(Math.random() * db.length)]);
+  res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  res.json(db[req.params.id - 1]);
+  res.json(db.testimonials[req.params.id - 1]);
 });
 
 app.post('/testimonials', (req, res) => {
   const { author, text } = req.body;
   const id = uuid();
   const newTestimonial = { id: id, author, text };
-  db.push(newTestimonial);
+  db.testimonials.push(newTestimonial);
   res.json({ message: 'OK' });
 });
 
 app.put('/testimonials/:id', (req, res) => {
   const { author, text } = req.body;
-  const testimonial = db[req.params.id - 1];
+  const testimonial = db.testimonials[req.params.id - 1];
   testimonial.author = author;
   testimonial.text = text;
   res.json({ message: 'OK' });
 });
 
 app.delete('/testimonials/:id', (req, res) => {
-  db.splice(req.params.id - 1, 1);
+  db.testimonials.splice(req.params.id - 1, 1);
   res.json({ message: 'OK' });
 });
+
+
+app.get('/concerts', (req, res) => {
+  res.json(db.concerts);
+});
+
+app.get('/concerts/:id', (req, res) => {
+  res.json(db.concerts[req.params.id - 1]);
+});
+
+app.post('/concerts', (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = uuid();
+  const newConcert = { id: id, performer, genre, price, day, image };
+  db.concerts.push(newConcert);
+  res.json({ message: 'OK' });
+});
+
+app.put('/concerts/:id', (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const concert = db.concerts[req.params.id - 1];
+  concert.performer = performer;
+  concert.genre = genre;
+  concert.price = price;
+  concert.day = day;
+  concert.image = image;
+  res.json({ message: 'OK' });
+});
+
+app.delete('/concerts/:id', (req, res) => {
+  db.concerts.splice(req.params.id - 1, 1);
+  res.json({ message: 'OK' });
+});
+
+
+app.get('/seats', (req, res) => {
+  res.json(db.seats);
+});
+
+app.get('/seats/:id', (req, res) => {
+  res.json(db.seats[req.params.id - 1]);
+});
+
+app.post('/seats', (req, res) => {
+  const { day, seat, client, email } = req.body;
+  const id = uuid();
+  const newSeat = { id: id, day, seat, client, email };
+  db.seats.push(newSeat);
+  res.json({ message: 'OK' });
+});
+
+app.put('/seats/:id', (req, res) => {
+  const { day, seat, client, email } = req.body;
+  const seatId = db.seats[req.params.id - 1];
+  seatId.day = day;
+  seatId.text = seat;
+  seatId.client = client;
+  seatId.email = email;
+  res.json({ message: 'OK' });
+});
+
+app.delete('/seats/:id', (req, res) => {
+  db.seats.splice(req.params.id - 1, 1);
+  res.json({ message: 'OK' });
+});
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
