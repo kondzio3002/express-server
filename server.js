aa945805cf78cfafdcc84d3f750120c9beb02fdd
Konrad Brożyna <kondzio3002@gmail.com>
2023-02-12 13:30:39 +0100

    Add middleware and array

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  { id: 3, author: 'Adam Snow', text: 'Successful cooperation!' },
+  { id: 4, author: 'Danny Nitro', text: 'It promise great cooperation.' },
+  { id: 5, author: 'Angelina Nobel', text: 'Make money!' },
+  { id: 6, author: 'Anna Maj', text: 'Good job.' },
+];
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
